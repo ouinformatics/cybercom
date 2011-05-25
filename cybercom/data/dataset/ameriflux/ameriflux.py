534b1c24befb5c61c792c4118d521acd064a123c
Jonah M. Duckles <jduckles@ou.edu>
2011-05-25 09:10:01 -0500

    Reformatted long lines and added docstrings

--- cybercom/data/dataset/ameriflux/ameriflux.py
+++ cybercom/data/dataset/ameriflux/ameriflux.py
@@ -1,19 +1,46 @@
+"""
+Functions for accessing ameriflux tower data stored in the cybercommons.
+
+Examples:
+
+Import module:
+>>> from cybercom.data.dataset import ameriflux
+>>> from datetime import datetime
+
+"""
+
+AMF_DB='amf_level4'
+
 from pymongo import Connection
-import numpy as np
+try:
+    import numpy as np
+except:
+    print "numpy is not installed, getvar(as_method='numpy') will not work"
 import datetime
+from cybercom.data.catalog import datalayer
 
-aggregation = 'weekly'
+aggregation = 'weekly' # Set default aggregation to weekly
+dbhost = 'fire.rccc.ou.edu'
 
-con = Connection()
-db = con['amf_level4']
+con = Connection(dbhost)
+db = con[AMF_DB]
 
-def aggregations(collection='amf_level4'):
-    """Show the levels of aggregation [hourly,dayly,weekly,monthly] available in the dataste
+def aggregations(collection=AMF_DB):
+    """Show the levels of aggregation [hourly,dayly,weekly,monthly] available 
+        in the dataset
+
+    Show aggregations available:
+    >>> ameriflux.aggregations()
+
     """
-    return [ item for item in con[collection].collection_names() if item != 'system.indexes'] 
+    return [ item for item in con[collection].collection_names() 
+                if item != 'system.indexes'] 
 
 def locations(aggregation=aggregation):
     """List locations available for a given aggregation level
+    
+    Show locations available at a given aggregation:
+    >>> ameriflux.locations('monthly')
     """
     if aggregation in aggregations():
        return db[aggregation].distinct('location')
@@ -22,6 +49,9 @@ def locations(aggregation=aggregation):
 
 def dates(location,aggregation=aggregation, limits=False):
     """Show date range of given location/aggregation pair
+
+    Show dates available for given aggregation and location:
+    >>> ameriflux.dates('US-FPE', 'monthly')
     """
     dates = db[aggregation].distinct('StartDate')
     if limits:
@@ -31,25 +61,68 @@ def dates(location,aggregation=aggregation, limits=False):
 
 def variables(aggregation=aggregation):
     """List variables available for a particular aggregation product
+    
+    Show variables available for a given aggregation:
+    >>> ameriflux.variables('weekly')
+
     """
     return db[aggregation].find_one().keys()
-    
 
-def getvar(location='US-FPE', variable='NEE_or_fANN', aggregation=aggregation, date_from=datetime.datetime(1990,1,1), date_to=datetime.datetime.now()):
+def locationinfo(location):
+    cat = datalayer.Metadata()
+    return cat.Search('dt_location', ['lat','lon','loc_id'], where="loc_id = '%s'" %(location))
+
+def getvar(location='US-FPE', 
+           variable='NEE_or_fANN', 
+           aggregation=aggregation, 
+           date_from=datetime.datetime(1990,1,1), 
+           date_to=datetime.datetime.now(),
+           as_method='dict'):
+    """Get an ameriflux observation timeseries from a given 
+        aggregation.
+    
+    Get weekly data from US-FPE tower:
+    >>> ameriflux.getvar('US-FPE', 'NEE_or_fANN', aggregation='weekly',
+                        date_from = datetime(1990,1,1),
+                        date_to = datetime(2011,2,28) )
+    """
     if aggregation in aggregations() and variable in variables():
         if aggregation == 'monthly':
             month = db['monthly']
-            cur = month.find({'location': location, 'StartDate': { '$gte': date_from }, 'EndDate': { '$lte': date_to } }, [variable, 'Month', 'StartDate'])
-            return np.array([ ( datetime.datetime.strftime(item['StartDate'], '%Y%j'), item['Month'], item[variable]) for item in cur ], dtype = {'names': ['StartDate', 'month', variable], 'formats': ['i4','i4','f4']} )
+            cur = month.find({'location': location, 
+                                'StartDate': { '$gte': date_from }, 
+                                'EndDate': { '$lte': date_to } }, 
+                                [variable, 'Month', 'StartDate'])
+            if as_method == 'numpy':
+                return np.array([ ( item['StartDate'], 
+                                    item['Month'], item[variable]) 
+                                for item in cur ], 
+                                dtype = {'names': ['date', 
+                                                    'month', 
+                                                    variable], 
+                                'formats': ['object','i4','f4']} ).view(np.recarray)
+            if as_method == 'dict':
+                return [ item for item in cur ]
         elif aggregation == 'weekly':
             week = db['weekly']
-            cur = week.find({'location': location, 'StartDate': { '$gte': date_from }, 'EndDate': { '$lte': date_to} }, [variable, 'Period', 'observed_year'])
-            return np.array( [ (item['observed_year'], item['Period'], item[variable]) for item in cur ], dtype = {'names':['year','week',variable], 'formats': ['i4','i4', 'f4'] })
+            cur = week.find({'location': location, 
+                             'StartDate': { '$gte': date_from }, 
+                             'EndDate': { '$lte': date_to} }, 
+                             [variable, 'Period', 'StartDate'])
+            if as_method == 'numpy':
+                return np.array( [ (item['StartDate'], item['Period'], 
+                                item[variable]) for item in cur ], 
+                                dtype = {'names':['date','week',variable], 
+                                'formats': ['object','i4', 'f4'] }).view(np.recarray)
+            if as_method == 'dict':
+                return [item for item in cur]
         elif aggregation == 'hourly':
             pass # not implemented
         elif aggregation == 'daily':
             pass # not implemented
     else:
-        print "Unknown aggregation or variable, know aggregations are " + str(aggregations()) + ' variables are: ' + str(variables())
+        out_msg = """Unknown aggregation or variable, know aggregations are %s
+           Variables are %s"""
+        print  out_msg % (aggregations(), variables())
 
 
