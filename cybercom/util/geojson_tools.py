b651ed46f354e65ca569b1453f0ceae3bd585f44
Jonah Duckles <jduckles@ou.edu>
2011-06-15 15:49:14 -0500

    Added reprojection

--- cybercom/util/geojson_tools.py
+++ cybercom/util/geojson_tools.py
@@ -1,10 +1,18 @@
 import geojson
+import pyproj
 
-def mkGeoJSONPoint(obj,latkey,lonkey,attributes=False):
+# Set Default Projection
+TARGET_PROJECTION="+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+SOURCE_PROJECTION="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+
+def mkGeoJSONPoint(obj,latkey,lonkey,attributes=False, t_srs=TARGET_PROJECTION, s_srs=SOURCE_PROJECTION):
     FC = []
     for item in obj:
         x = item.pop(lonkey)
         y = item.pop(latkey)
+        p1 = pyproj.Proj(s_srs)
+        p2 = pyproj.Proj(t_srs)
+        x, y = pyproj.transform( p1, p2, x, y)
         if attributes:
             FC.append( geojson.Feature( geometry=geojson.Point((x,y)), properties = item ) )
         else:
