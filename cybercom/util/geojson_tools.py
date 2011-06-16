d0ce580315ccd8cdc5b40379375b3f3ca4fe58bc
Jonah Duckles <jduckles@ou.edu>
2011-06-16 08:12:11 -0500

    changed default target

--- cybercom/util/geojson_tools.py
+++ cybercom/util/geojson_tools.py
@@ -2,7 +2,7 @@ import geojson
 import pyproj
 
 # Set Default Projection
-TARGET_PROJECTION="+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+TARGET_PROJECTION="+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 SOURCE_PROJECTION="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 
 def mkGeoJSONPoint(obj,latkey,lonkey,attributes=False, t_srs=TARGET_PROJECTION, s_srs=SOURCE_PROJECTION):
