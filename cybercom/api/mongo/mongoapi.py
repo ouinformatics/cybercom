6121f2615d1c274bff32347198694ef23ae04649
Jonah Duckles <jduckles@ou.edu>
2011-08-25 16:54:53 -0500

    API hook for callback

--- cybercom/api/mongo/mongoapi.py
+++ cybercom/api/mongo/mongoapi.py
@@ -24,8 +24,8 @@ class Root(object):
         return find(db, col, query, callback, showids, date)
     @cherrypy.expose
     @mimetype('application/json')
-    def find_loc(self, db=None, col=None, x='lon', y='lat', idcol='_id', properties=False, **kwargs):
-        return find_loc( db, col, x, y, idcol, properties)
+    def find_loc(self, db=None, col=None, x='lon', y='lat', idcol='_id', properties=False, callback = None, **kwargs):
+        return find_loc( db, col, x, y, idcol, properties, callback)
 
 cherrypy.tree.mount(Root())
 application = cherrypy.tree
