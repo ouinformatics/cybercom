e7710034cf3b429207b58a8564f4fa8e48b60b10
Mark Stacy <markstacy@ou.edu>
2012-02-10 14:16:00 -0600

    add group by to mongo api

--- cybercom/api/mongo/mongoapi.py
+++ cybercom/api/mongo/mongoapi.py
@@ -1,7 +1,7 @@
 import cherrypy
 import json 
-from cybercom.data.mongo.get import find, find_loc
-
+#from cybercom.data.mongo.get import find,group, find_loc
+from get import find,group, find_loc
 def mimetype(type):
     def decorate(func):
         def wrapper(*args, **kwargs):
@@ -23,6 +23,14 @@ class Root(object):
         Wrapper for underlying pymongo access
         """
         return find(db, col, query, callback, showids, date)
+    @cherrypy.expose
+    @mimetype('application/json')
+    @cherrypy.tools.gzip()
+    def group_by(self, db=None, col=None,key=None,variable=None, query=None,callback=None, **kwargs):
+        """ 
+        Wrapper for underlying pymongo access
+        """
+        return group(db, col, key,variable,query,callback)
     @cherrypy.expose
     @mimetype('application/json')
     def find_loc(self, db=None, col=None, x='lon', y='lat', idcol='_id', properties=False, query=None, callback=None, **kwargs):
